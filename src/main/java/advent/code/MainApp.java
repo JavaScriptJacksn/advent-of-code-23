f7a76fd219d859ad158067bf5f6599a2009125bc
JavaScriptJacksn <jbjackson247@gmail.com>
2023-12-09 18:45:32 +0000

    tidy main method

--- src/main/java/advent/code/MainApp.java
+++ src/main/java/advent/code/MainApp.java
@@ -1,12 +1,21 @@
 package advent.code;
 
 import advent.code.solutions.DayOneTrebuchet;
+import advent.code.solutions.Solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainApp {
 
     public static void main (String[] args){
-        DayOneTrebuchet dayOne = new DayOneTrebuchet();
-        dayOne.run();
+
+        List<Solution> solutionsToRun = Arrays.asList(
+                new DayOneTrebuchet()
+        );
+
+        solutionsToRun.forEach(Solution::run);
     }
 
 }
